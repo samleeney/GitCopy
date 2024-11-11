68e3f448963082ddca198204af8fb258f047f2b4
Sam Leeney <sakl2@cam.ac.uk>
2024-11-11 10:27:25 +0000

    auto install  requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
+import os
+
+
+# Read the contents of requirements.txt
+def read_requirements(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
 
 setup(
     name="GitCopy",
     version="0.1",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=read_requirements("requirements.txt"),
     entry_points={
         "console_scripts": [
             "gitcopy=gitcopy.gitcopy:main",
